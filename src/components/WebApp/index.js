1fb252b89664bfad17c1849ace639b7d5a2a71c0
anitha-akkimi <anithaakkimi666@gmail.com>
2023-05-02 16:50:26 +0530

    second edit

--- src/components/WebApp/index.js
+++ src/components/WebApp/index.js
@@ -2,7 +2,7 @@ import './index.css'
 
 const WebApp = () => <div>
     <h1>Web App Application</h1>
-    <p>Webapp will provide application on a browser as well as in android applications</p>
+    <p>Webapp will provide application on a browser as well as in android applications in a smart phone</p>
 </div>
 
 
