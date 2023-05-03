5c29be6f3d51440f6058d2f3ab8092e1e6ec674e
anitha-akkimi <anithaakkimi666@gmail.com>
2023-05-03 12:17:37 +0530

    fourth edit

--- src/components/WebApp/index.js
+++ src/components/WebApp/index.js
@@ -1,6 +1,6 @@
 import './index.css'
 
-const WebApp = () => <div>
+const WebApp = () => <div className='bg-container'>
     <h1>Web App Application</h1>
     <p>Webapp will provide application on a browser as well as in android applications.</p>
 </div>
